fea5c061160191564d8cf5e20f7decd7e6f6391d
Ali Can MOGOL <alicanmogol@gmail.com>
2013-12-30 13:57:22 +0200

    files to delete added

---
+++ SeatMapViewer/viewcontrollers/CMDeleteMe.h
@@ -0,0 +1,13 @@
+//
+//  CMDeleteMe.h
+//  SeatMapViewer
+//
+//  Created by Ali Can MOGOL on 30/12/13.
+//  Copyright (c) 2013 Can A. MOGOL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CMDeleteMe : NSObject
+
+@end
